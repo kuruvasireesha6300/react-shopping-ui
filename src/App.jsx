eab7f77f75101ee3f2cabd081f9bae7d5ad55312
sireesha <kuruvasireesha064@gmail.com>
2024-08-04 09:52:11 -0700

    shopping

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainPage from './shoppingfolder/pages/MainPage'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      
+      <MainPage/>
+    </div>
+  )
+}
+
+export default App
